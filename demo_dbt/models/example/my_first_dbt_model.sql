54b06119be1c1a3a965781c9cd2ff6e0e8843194
Sanjay Chauhan <sanjay.chauhan@tredence.com>
2021-12-02 16:14:41 +0530

    first commit

--- demo_dbt/models/example/my_first_dbt_model.sql
+++ demo_dbt/models/example/my_first_dbt_model.sql
@@ -13,6 +13,8 @@ with source_data as (
 
     select 1 as id
     union all
+    select 2 as id
+    union all
     select null as id
 
 )
@@ -23,5 +25,4 @@ from source_data
 /*
     Uncomment the line below to remove records with null `id` values
 */
-
 -- where id is not null
